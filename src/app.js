d4691336a8903650f7c0182f50681961e854f564
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-12-23 05:46:06 +0600

    utils code : done

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: "16kb"
+}));
+
+app.use(express.static("public"))
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+export { app };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running at ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("Database connection error: ", err)
+    })
 
 
 
