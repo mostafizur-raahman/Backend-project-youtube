d7cda0d5f1ae7701e695225a0e1055ea17e6bf9f
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-12-26 16:48:49 +0600

    cloudinary and multer middleware implementations

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLODINARI_CLOUD_NAME,
+    api_key: process.env.CLODINARI_CLOUD_API_KEY,
+    api_secret: process.env.CLODINARI_CLOD_SECRET_KEY
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been upload successfully 
+        console.log("file has been uploaded successfully", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
